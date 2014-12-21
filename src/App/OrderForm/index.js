32fac109439bb94aae8a081e2036b8aec41ef7b7
Mark Ryall <mark@ryall.name>
2014-12-21 13:41:24 +1000

    extracted order form to a separate component

---
+++ src/App/OrderForm/index.js
@@ -0,0 +1,45 @@
+var React = require('react');
+var SelectInput = require('../../inputs/SelectInput');
+var TextInput = require('../../inputs/TextInput');
+var emitter = require('../../emitter');
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      current: {
+        product: '',
+        quantity: 1
+      }
+    };
+  },
+
+  onChange: function(event) {
+    var id = event.target.id;
+    var value = event.target.value;
+    var current = this.state.current || {};
+    current[id] = value;
+    this.setState({current: current});
+  },
+
+  submit: function(event) {
+    emitter.emit('addItem', this.state.current);
+    this.setState(this.getInitialState());
+    event.preventDefault();
+  },
+
+  render: function() {
+    var current = this.state.current;
+    var products = [
+      '',
+      'Crème brûlée',
+      'White Chocolate and Raspberry Muffin',
+      'Croissant with Jam',
+      'Chocolate Éclair'
+    ];
+    return <form className="orderForm" role="form" onSubmit={ this.submit }>
+      <SelectInput id="product" label="Product" onChange={ this.onChange } value={ current.product } options={ products } />
+      <TextInput id="quantity" label="Quantity" onChange={ this.onChange } value={ current.quantity } />
+      <button id="submit" type="submit" className="btn btn-primary">Add</button>
+    </form>;
+  }
+});
--- src/App/index.js
+++ src/App/index.js
@@ -1,47 +1,10 @@
 var React = require('react');
-var SelectInput = require('../inputs/SelectInput');
-var TextInput = require('../inputs/TextInput');
-var emitter = require('../emitter');
+var OrderForm = require('./OrderForm');
 
 module.exports = React.createClass({
-  getInitialState: function() {
-    return {
-      current: {
-        product: '',
-        quantity: 1
-      }
-    };
-  },
-
-  onChange: function(event) {
-    var id = event.target.id;
-    var value = event.target.value;
-    var current = this.state.current || {};
-    current[id] = value;
-    this.setState({current: current});
-  },
-
-  submit: function(event) {
-    emitter.emit('addItem', this.state.current);
-    this.setState(this.getInitialState());
-    event.preventDefault();
-  },
-
   render: function() {
-    var current = this.state.current;
-    var products = [
-      '',
-      'Crème brûlée',
-      'White Chocolate and Raspberry Muffin',
-      'Croissant with Jam',
-      'Chocolate Éclair'
-    ];
     return <div className="content">
-        <form className="orderForm" role="form" onSubmit={ this.submit }>
-          <SelectInput id="product" label="Product" onChange={ this.onChange } value={ current.product } options={ products } />
-          <TextInput id="quantity" label="Quantity" onChange={ this.onChange } value={ current.quantity } />
-          <button id="submit" type="submit" className="btn btn-primary">Add</button>
-        </form>
+        <OrderForm />
       </div>;
   }
 });
